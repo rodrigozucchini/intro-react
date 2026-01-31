97ff272107c4925b7c4427335829938d9a88da03
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-01-31 23:47:07 +0000

    add components

---
+++ src/TodoIcon.jsx
@@ -0,0 +1,25 @@
+import CheckSVG from './check.svg?react';
+import DeleteSVG from './delete.svg?react';
+import './TodoIcon.css';
+
+const iconTypes = {
+  check: (color) => (
+    <CheckSVG className="Icon-svg" fill={color} />
+  ),
+  delete: (color) => (
+    <DeleteSVG className="Icon-svg" fill={color} />
+  ),
+};
+
+function TodoIcon({ type, color, onClick }) {
+  return (
+    <span
+      className={`Icon-container Icon-container-${type}`}
+      onClick={onClick}
+    >
+      {iconTypes[type](color)}
+    </span>
+  );
+}
+
+export { TodoIcon };
