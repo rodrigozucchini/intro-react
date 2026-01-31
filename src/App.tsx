8b8f594f34a7ed861e9917b96468902b9465f1fd
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-01-31 03:05:55 +0000

    add counting TODO'S

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TodoItem } from './TodoItem/TodoItem';
 import { TodoList } from './TodoList/TodoList';
 import { TodoSearch } from './TodoSearch/TodoSearch';
 import { CreateTodoButton } from './CreateTodoButton/CreateTodoButton';
+import React from 'react';
 
 const defaultTodos: { text: string; completed: boolean; }[] = [
   { text: 'Cortar cebolla', completed: true},
@@ -13,10 +14,24 @@ const defaultTodos: { text: string; completed: boolean; }[] = [
 ];
 
 function App() {
+
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo=> todo.completed === true).length;
+  const totalTodos = defaultTodos.length;
+
   return (
     <>
-      <TodoCounter completed={15} total={16} />
-      <TodoSearch />
+      <TodoCounter 
+        completed={completedTodos} 
+        total={totalTodos} 
+      />
+
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {defaultTodos.map(todo => (
--- src/TodoSearch/TodoSearch.tsx
+++ src/TodoSearch/TodoSearch.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({
+  searchValue,
+  setSearchValue,
+}) {
 
     return (
       <input 
