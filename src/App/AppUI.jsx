3e4d807a8410bce0fb1790c32435377b6a3dd071
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-02-11 03:43:21 +0000

    add css and TodoForm

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -9,6 +9,7 @@ import { EmptyTodos } from '../EmptyTodo';
 import { TodoContext } from '../TodoContext';
 import { Modal } from '../Modal/Index';
 import React from 'react';
+import { TodoForm } from '../TodoForm';
 
 function AppUI() {
   const {
@@ -54,7 +55,7 @@ function AppUI() {
 
       {openModal && (
         <Modal>
-          La funcionalidad de agregar TODO
+          <TodoForm />
         </Modal>
       )}
     </>
--- src/TodoCounter/index.jsx
+++ src/TodoCounter/index.jsx
@@ -9,7 +9,7 @@ function TodoCounter() {
 
   return (
     <h1 className="TodoCounter">
-      Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> TODOs
+      Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> Tareas
     </h1>
   );
 }
